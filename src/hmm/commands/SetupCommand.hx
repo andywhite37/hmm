282cfba3bf13cf0f4dd78150eecfcc9cea89f42a
Andy White <greatwhitehope@gmail.com>
2015-09-24 16:24:07 -0600

    Submit to haxelib

--- src/hmm/commands/SetupCommand.hx
+++ src/hmm/commands/SetupCommand.hx
@@ -14,6 +14,7 @@ class SetupCommand implements ICommand {
   }
 
   public function run() {
+    Shell.command("chmod", ["+x", getHmmScriptFilePath()]);
     Shell.symbolicLink(getHmmScriptFilePath(), HMM_LINK_PATH);
   }
 
